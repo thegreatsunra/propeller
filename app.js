3e6611619a5905c49b96f7d7d0b5a2c95ba06acb
Dane Petersen <thegreatsunra@gmail.com>
2013-09-11 20:50:57 -0700

    Install hbs and switch rendering engine to Handlebars

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'hjs');
+app.set('view engine', 'html.hbs');
+app.engine('html.hbs', require('hbs').__express);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());

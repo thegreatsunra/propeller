3da36eab729292db2425b08ee6a5d6f425da4b65
Dane Petersen <thegreatsunra@gmail.com>
2013-09-13 17:28:07 -0700

    Define dynamic routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,20 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Home' });
+  res.locals.title = 'Express Prototyper Home';
+  res.locals.slug = 'home';
+  // res.locals.context = context; // defined in models/index.js
+  res.render('home');
 };
+
+/*
+ * GET all other views
+ */
+
+exports.views = function (req, res){
+  var requestedView = req.params.view.replace('.html', '');
+  res.locals.title = requestedView;
+  res.locals.slug = requestedView;
+  // res.locals.context = context; // defined in models/index.js
+  res.render(requestedView);
+}

5cb94e7f9ff4e9bdbd3ad8c26e4e7a2978b64f5d
Dane Petersen <thegreatsunra@gmail.com>
2013-09-13 17:22:51 -0700

    Add random value for cookieParser

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('QnISSjxBj0qmdYXLxeVp'));
 app.use(express.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));

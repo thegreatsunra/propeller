4069544545c18fb6987c3dfa202d9c8b0272d1a6
Dane Petersen <thegreatsunra@gmail.com>
2013-09-13 17:24:26 -0700

    Remove less-middleware and other unnecessary stuff

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.engine('hbs', require('hbs').__express);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -28,7 +26,6 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('QnISSjxBj0qmdYXLxeVp'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Define handlebars helpers

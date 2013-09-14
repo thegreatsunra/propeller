c289fd7b3a6736cd8e58819e1406987860401a2c
Dane Petersen <thegreatsunra@gmail.com>
2013-09-14 07:34:32 -0700

    Add context object model with placeholder data

---
+++ models/index.js
@@ -0,0 +1,24 @@
+context = {
+  people: [
+    {
+      first_name: 'Steve',
+      last_name: 'Jobs',
+      company: 'Apple'
+    },
+    {
+      first_name: 'Steve',
+      last_name: 'Wozniak',
+      company: 'Apple'
+    },
+    {
+      first_name: 'Bill',
+      last_name: 'Gates',
+      company: 'Microsoft'
+    },
+    {
+      first_name: 'Steve',
+      last_name: 'Ballmer',
+      company: 'Microsoft'
+    }
+  ]
+};

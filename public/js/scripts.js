b8a5c94b2f0c8981848b563f6a2778a1f847c685
Dane Petersen <thegreatsunra@gmail.com>
2013-09-13 17:26:08 -0700

    Add jQuery .html() function to scripts.js example

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,5 @@
 // Scripts go here
 
+  $(function() {
+    $('.lead').html('Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.');
+  });

19dbc45d7bc5f29f001bfd0be059207348d619ad
Dane Petersen <thegreatsunra@gmail.com>
2013-09-21 10:24:37 -0700

    Add KnockoutJS example to home screen

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,3 +3,16 @@
   $(function() {
     $('.lead').html('Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.');
   });
+
+  // Here's my data model
+  var ViewModel = function(first, last) {
+      this.firstName = ko.observable(first);
+      this.lastName = ko.observable(last);
+   
+      this.fullName = ko.computed(function() {
+          // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+          return this.firstName() + " " + this.lastName();
+      }, this);
+  };
+   
+  ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work

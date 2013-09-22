e67a4737f799f847e58578cce67bbca1db60deb3
Dane Petersen <thegreatsunra@gmail.com>
2013-09-22 13:55:48 -0700

    Put knockout, bootstrap and jquery examples into separate requirejs modules

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,39 @@
-//Load common code that includes config
+// load requirejs config file
 require(['./require.config'], function (common) {
+
+  // test jquery by appending HTML content
   require([
     'jquery', 
-    'bootstrap',
-    'knockout',
-    'viewmodel'
-    ], function($, _bootstrap, ko, HelloViewModel){
+    ], function($){
       'use strict';
-      // this is where all the site code should begin
-
-
+      // this is where the code goes
       $(function() {
         $('.lead').html('Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.');
       });
-
-      ko.applyBindings(new HelloViewModel("Planet", "Earth")); // This makes Knockout get to work
-
   });
+
+  // test knockout by binding hello world view model
+  require([
+    'knockout',
+    'viewmodel'
+    ], function(ko, HelloViewModel){
+      'use strict';
+      // this is where the code goes
+      ko.applyBindings(new HelloViewModel("Planet", "Earth")); // This makes Knockout get to work
+  });
+
+  // add bootstrap javascript
+  require([
+    'jquery',
+    'bootstrap'
+    ], function($, bootstrap){
+      'use strict';
+      // any JavaScript that requires bootstrap should go here
+      //
+      //
+      //
+  });
+
 });
 
 

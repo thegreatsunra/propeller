aa3bbf22dadd33831e8bb74264a48995a663af81
Dane Petersen <thegreatsunra@gmail.com>
2013-09-23 18:07:50 -0700

    Add compiled Knockout. Add cparker's CSV to JSON

--- public/js/require.config.js
+++ public/js/require.config.js
@@ -3,7 +3,8 @@ require.config({
   paths: {
     jquery: 'bower_components/bootstrap/assets/js/jquery',
     bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min',
-    knockout: 'bower_components/knockoutjs/build/output/knockout-latest.debug',
+    knockout: 'bower_components/knockout.js/knockout-2.3.0.debug',
+    csvtojson: 'bower_components/csv-to-json/src/js/csv-to-json',
     scripts: 'js/scripts',
     viewmodel: 'js/viewmodel'
   },

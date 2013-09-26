66449350cf2194376ed594bf84dc0b069d62fcde
Dane Petersen <thegreatsunra@gmail.com>
2013-09-25 19:56:03 -0700

    Generate static dist

--- dist/js/require.config.js
+++ dist/js/require.config.js
@@ -3,7 +3,8 @@ require.config({
   paths: {
     jquery: 'bower_components/bootstrap/assets/js/jquery',
     bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min',
-    knockout: 'bower_components/knockoutjs/build/output/knockout-latest.debug',
+    knockout: 'bower_components/knockout.js/knockout-2.3.0.debug',
+    'csv-js' : 'bower_components/csv-js/src/csv',
     scripts: 'js/scripts',
     viewmodel: 'js/viewmodel'
   },

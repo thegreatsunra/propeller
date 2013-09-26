24ea2503cc8e5e9ad06df13bcc00fe57463ee018
Dane Petersen <thegreatsunra@gmail.com>
2013-09-25 19:26:35 -0700

    Replace csv-to-json with csv-js

--- public/js/require.config.js
+++ public/js/require.config.js
@@ -4,7 +4,7 @@ require.config({
     jquery: 'bower_components/bootstrap/assets/js/jquery',
     bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min',
     knockout: 'bower_components/knockout.js/knockout-2.3.0.debug',
-    csvtojson: 'bower_components/csv-to-json/src/js/csv-to-json',
+    'csv-js' : 'bower_components/csv-js/src/csv',
     scripts: 'js/scripts',
     viewmodel: 'js/viewmodel'
   },

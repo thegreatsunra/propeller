13f8a4a0cf1cc6ca2a3b871f16a3396d25a579c2
Dane Petersen <thegreatsunra@gmail.com>
2013-09-25 19:27:05 -0700

    Add front-end csv parsing example with knockout

--- public/js/viewmodel.js
+++ public/js/viewmodel.js
@@ -1,9 +1,10 @@
-define(['knockout'], function (ko) {
+define(['jquery', 'knockout', 'csv-js'], function ($, ko, CSV) {
   'use strict';
 
   // Here's my data model
   var ViewModel = function(first, last) {
     var self = this;
+    self.places = ko.observable(CSVtoJSON('places'));
     self.firstName = ko.observable(first);
     self.lastName = ko.observable(last);
   
@@ -12,6 +13,23 @@ define(['knockout'], function (ko) {
       return self.firstName() + " " + self.lastName();
     }, self);
   };
-
   return ViewModel;
+
+  function CSVtoJSON(data) {
+    var dataPath = 'data/'
+      , extension = '.csv'
+      , dataURL = dataPath + data + extension
+      , output = null;
+    $.ajax({
+      url: dataURL,
+      type: 'get',
+      datatype: 'text',
+      async: false,
+      success: function(data) {
+        output = CSV.parse(data)
+      }
+    });
+    return output;
+  };
+
 });

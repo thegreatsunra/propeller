2ee923b323ba13c6798f50795b2ae9058a1b10d1
Dane Petersen <thegreatsunra@gmail.com>
2013-10-17 06:59:55 -0700

    Generate static dist files for D3, RequireJS, Underscore

--- dist/js/require.config.js
+++ dist/js/require.config.js
@@ -2,15 +2,17 @@ require.config({
   baseUrl: '',
   paths: {
     jquery: 'bower_components/bootstrap/assets/js/jquery',
-    bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min',
+    bootstrap: 'bower_components/bootstrap/dist/js/bootstrap',
     knockout: 'bower_components/knockout.js/knockout-2.3.0.debug',
-    'csv-js' : 'bower_components/csv-js/src/csv',
-    'moment' : 'bower_components/moment/moment',
-    'd3' : 'bower_components/d3/d3',
+    'csv-js': 'bower_components/csv-js/src/csv',
+    moment: 'bower_components/moment/moment',
+    d3: 'bower_components/d3/d3',
+    underscore: 'bower_components/underscore/underscore',
     scripts: 'js/scripts',
     viewmodel: 'js/viewmodel'
   },
   shim: {
-  'bootstrap':{deps: ['jquery']}
+  bootstrap: {deps: ['jquery']},
+  underscore: {exports: '_'}
   }
 });

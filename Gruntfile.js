55caa512bf0ba7ba70f8a06c36b5fd86fc326bb0
Dane Petersen <dane@Siskiyou.local>
2014-01-12 05:04:33 -0800

    Add post-process to prettify outputted HTML

--- Gruntfile.js
+++ Gruntfile.js
@@ -231,7 +231,8 @@ module.exports = function (grunt) {
     assemble: {
       options: {
         plugins: ['assemble-contrib-permalinks'],
-        helpers: ['<%= config.src %>/assemble/helpers/{,*/}*.js']
+        helpers: ['<%= config.src %>/assemble/helpers/{,*/}*.js'],
+        postprocess: require('pretty')
       },
       pages: {
         options: {
